3e0c6a79d829547e0b2b03773e7a5b9755690fee
Peter Murphy <26548438+ptrfrncsmrph@users.noreply.github.com>
2020-12-20 09:55:17 -0500

    Add notes to lift exercises

--- src/validation/1-apply/04a-lift.ts
+++ src/validation/1-apply/04a-lift.ts
@@ -6,16 +6,20 @@ import * as O from "fp-ts/lib/Option"
 import * as RA from "fp-ts/lib/ReadonlyArray"
 import * as T from "fp-ts/lib/Task"
 
-// "Generalizing" over F, which always needs to have kind Type -> Type
+// "Generalizing" over F, which always needs to have kind Type -> Type. Because
+// we don't really have HKT's in TypeScript, I'm going to use these global
+// variables as a stand-in.
 type F<A> = O.Option<A>
 const F = O
 
 type TODO = any
 
 //    lift1 :: (a -> b) -> f a -> f b
+//    👆 This is the same as Functor's `map` method
 const lift1 = <A, B>(f: (a: A) => B) => (fa: F<A>): F<B> => undefined as TODO
 
 //    lift2 :: (a -> b -> c) -> f a -> f b -> f c
+//    👆 We can't do this with `map` alone, need Apply's `ap`
 const lift2 = <A, B, C>(f: (a: A) => (b: B) => C) => (fa: F<A>) => (
   fb: F<B>,
 ): F<C> => undefined as TODO
